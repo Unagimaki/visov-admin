2681141eb7916210b67363ae60e0044c69cb5bca
Unagimaki <134698209+Unagimaki@users.noreply.github.com>
2024-11-29 17:21:31 +0400

    Update PanelContainer.jsx

--- src/components/Panel/PanelContainer.jsx
+++ src/components/Panel/PanelContainer.jsx
@@ -1,12 +1,22 @@
 import { useSelector } from 'react-redux'
 import styles from './panelContainer.module.scss'
+import * as XLSX from 'xlsx';
 
 export const PanelContainer = () => {
     const answers = useSelector(state => state.answers)
+    const exportData = () => {
+        // Создание рабочей книги
+        const worksheet = XLSX.utils.json_to_sheet(answers); // Конвертация JSON в таблицу
+        const workbook = XLSX.utils.book_new(); // Создание новой книги
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1'); // Добавление таблицы
 
+        // Генерация файла Excel
+        XLSX.writeFile(workbook, 'ExportedData.xlsx'); // Скачивание файла
+    };
     return(
         <div className={styles.container}>
             <div>Количество ответов: {answers.length}</div>
+            <button onClick={exportData}>Скачать таблицу</button>
         </div>
     )
 }
